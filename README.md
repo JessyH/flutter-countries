be80bf3eb5a1d31be0e7e53d56a1ec8e9bdcbc85
Jessy HAVARD <havard.jessy@gmail.com>
2021-03-30 17:06:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Countries 🌍
+
+Countries is a Flutter application developed as an assignment for [SafeNow](https://www.safenow.app/).
---
+++ android/app/src/main/kotlin/com/jhavard/flutter_countries/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.jhavard.flutter_countries
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
